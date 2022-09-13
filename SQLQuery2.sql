8ece66f132ab90ad767cc4936f949668950330d7
dorKurant <113186627+dorKurant@users.noreply.github.com>
2022-09-13 16:04:18 +0300

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,6 +0,0 @@
-select c.City,number_of_cumpany=count(*), number_of_mikods=count (distinct c.PostalCode) 
-from Customers as c
-where c.CompanyName like '%e%' 
-group by c.City
-having COUNT (*) >1
-order by  number_of_cumpany desc
